95f6a1adc4e7055af4d270d57f15e9aea09c604c
Torkey <52922396+muhammed-torkey@users.noreply.github.com>
2021-06-18 12:49:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const http = require("http").Server(app);
 // require the socket.io module
 const io = require("socket.io");
 
-const port = 5000;
+const port = 80;
 
 //bodyparser middleware
 app.use(bodyParser.json());

e4512a27e0c2c29a487830e3a98ff129059c32fa
Thavamanirajasakthivel <thavamani.raja@hpe.com>
2022-07-15 13:32:22 +0530

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const db_host = process.env.MONGO_IP
+const db_host = process.env.MONGODB_IP
 
 const url = "mongodb://"+db_host+":27017/chat";
 
